d0079fce5f99f97318a643c87c43ed0ed8f9f60d
Aneurin Barker Snook <a@aneur.in>
2021-08-31 21:14:19 +0100

    feat: adopt, convert to typescript

--- lib/index.ts
+++ lib/index.ts
@@ -10,9 +10,24 @@
 // © 2021 Edge Network  \$$$$$$  |
 //   Technologies Ltd.   \______/
 
-const SHA256 = require('crypto-js/sha256')
-const keccak256 = require('js-sha3').keccak256
-const elliptic = require('elliptic')
+import SHA256 from 'crypto-js/sha256'
+import elliptic from 'elliptic'
+import { keccak256 } from 'js-sha3'
+
+type Address = string
+type ChecksumAddress = Address
+type Message = string
+type PrivateKey = Uint8Array | Buffer | string | number[] | elliptic.ec.KeyPair
+type PublicKey = string
+type SigningPrivateKey = Buffer | elliptic.ec.KeyPair
+type Signature = string
+
+type Wallet = {
+  address: Address
+  privateKey: PrivateKey
+  publicKey: PublicKey
+}
+
 const ec = new elliptic.ec('secp256k1')
 
 //
@@ -22,7 +37,7 @@ function generateKeyPair() {
   return ec.genKeyPair()
 }
 
-function generateWallet() {
+export function generateWallet(): Wallet {
   const keyPair = generateKeyPair()
   const privateKey = keyPair.getPrivate('hex').toString()
   const publicKey = keyPair.getPublic(true, 'hex').toString()
@@ -30,7 +45,7 @@ function generateWallet() {
   return { privateKey, publicKey, address }
 }
 
-function generateChecksumAddress(address) {
+export function generateChecksumAddress(address: Address): ChecksumAddress {
   const addr = address.slice(3)
   const addrHash = keccak256(addr.toLowerCase())
 
@@ -43,7 +58,7 @@ function generateChecksumAddress(address) {
   return `xe_${chkAddr}`
 }
 
-function checksumAddressIsValid(address) {
+export function checksumAddressIsValid(address: Address): boolean {
   if (!/^(xe_[a-fA-F0-9]{40})$/.test(address)) return false
   if (address !== generateChecksumAddress(address)) return false
   return true
@@ -52,22 +67,22 @@ function checksumAddressIsValid(address) {
 //
 // Conversion
 //
-function publicKeyToChecksumAddress(publicKey) {
+export function publicKeyToChecksumAddress(publicKey: PublicKey): ChecksumAddress {
   const hash = keccak256(publicKey)
   const addr = 'xe_' + hash.substring(hash.length - 40, hash.length)
   return generateChecksumAddress(addr)
 }
 
-function privateKeyToPublicKey(privateKey) {
+export function privateKeyToPublicKey(privateKey: PrivateKey): PublicKey {
   return ec.keyFromPrivate(privateKey, 'hex').getPublic(true, 'hex')
 }
 
-function privateKeyToChecksumAddress(privateKey) {
+export function privateKeyToChecksumAddress(privateKey: PrivateKey): ChecksumAddress {
   const publicKey = privateKeyToPublicKey(privateKey)
   return publicKeyToChecksumAddress(publicKey)
 }
 
-function xeStringFromMicroXe(mxe, format) {
+export function xeStringFromMicroXe(mxe: number, format: boolean): string {
   const s = mxe.toString()
   const fraction = s.substr(-6, 6).padStart(6, '0')
   let whole = s.substr(0, s.length - 6) || '0'
@@ -75,7 +90,7 @@ function xeStringFromMicroXe(mxe, format) {
   return `${whole}.${fraction}`
 }
 
-function toMicroXe(xe) {
+export function toMicroXe(xe: string|number): number {
   const s = typeof xe === 'number' ? xe.toString() : xe
   const parts = s.split('.')
   const whole = parts[0]
@@ -83,7 +98,7 @@ function toMicroXe(xe) {
   return parseInt(`${whole}${fraction}`)
 }
 
-function formatXe(xe, format) {
+export function formatXe(xe: string|number, format: boolean): string {
   const mxe = toMicroXe(xe)
   return xeStringFromMicroXe(mxe, format)
 }
@@ -91,23 +106,25 @@ function formatXe(xe, format) {
 //
 // Signatures
 //
-function generateSignature(privateKey, msg) {
+export function generateSignature(privateKey: SigningPrivateKey, msg: Message): Signature {
   const msgHash = SHA256(msg).toString()
   const msgHashByteArray = elliptic.utils.toArray(msgHash, 'hex')
   const signatureObj = ec.sign(msgHashByteArray, privateKey, 'hex', { canonical: true })
   const r = signatureObj.r.toString('hex', 32)
   const s = signatureObj.s.toString('hex', 32)
-  const i = signatureObj.recoveryParam.toString(16).padStart(2, '0')
+  const i = (typeof signatureObj.recoveryParam === 'number')
+    ? signatureObj.recoveryParam.toString(16).padStart(2, '0')
+    : ''
   return r + s + i
 }
 
-function verifySignatureAddress(msg, signature, address) {
+export function verifySignatureAddress(msg: string, signature: Signature, address: Address): boolean {
   const publicKey = recoverPublicKeyFromSignedMessage(msg, signature)
   const derivedAddress = publicKeyToChecksumAddress(publicKey)
   return address === derivedAddress
 }
 
-function recoverPublicKeyFromSignedMessage(msg, signature) {
+export function recoverPublicKeyFromSignedMessage(msg: Message, signature: Signature): PublicKey {
   const signatureObj = { r: signature.slice(0, 64), s: signature.slice(64, 128) }
   const recoveryParam = parseInt(signature.slice(128, 130), 16)
   const msgHash = SHA256(msg).toString()
@@ -116,25 +133,8 @@ function recoverPublicKeyFromSignedMessage(msg, signature) {
   return publicKey.encode('hex', true)
 }
 
-function recoverAddressFromSignedMessage(msg, signature) {
+export function recoverAddressFromSignedMessage(msg: Message, signature: Signature): Address {
   const publicKey = recoverPublicKeyFromSignedMessage(msg, signature)
   const derivedAddress = publicKeyToChecksumAddress(publicKey)
   return derivedAddress
-}
-
-module.exports = {
-  generateKeyPair,
-  generateWallet,
-  generateChecksumAddress,
-  checksumAddressIsValid,
-  publicKeyToChecksumAddress,
-  privateKeyToChecksumAddress,
-  privateKeyToPublicKey,
-  generateSignature,
-  verifySignatureAddress,
-  recoverPublicKeyFromSignedMessage,
-  recoverAddressFromSignedMessage,
-  xeStringFromMicroXe,
-  toMicroXe,
-  formatXe
 }
